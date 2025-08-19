a58f11b8c97c823964d2a31750fc0fbfdc612430
AnnaOnis <onischenko.anna11@gmail.com>
2025-08-19 11:59:49 +0300

    Правки Dockerfile и настроек Kafka

--- AdvertisingPlatforms.Kafka/Models/KafkaSettings.cs
+++ AdvertisingPlatforms.Kafka/Models/KafkaSettings.cs
@@ -1,3 +1,5 @@
+using Confluent.Kafka;
+
 namespace AdvertisingPlatforms.Kafka.Models
 {
     public class KafkaSettings
@@ -6,12 +8,14 @@ namespace AdvertisingPlatforms.Kafka.Models
         public string ComandTopic { get; set; } = null!;
         public string DataTopic { get; set; } = null!;
         public string ConsumerGroupId { get; set; } = null!;
-        public bool EnableAutoCreateTopics { get; set; } = true;
-
-
-        // Настройки для батчевой обработки
+        public string ClientId { get; set; } = null!;
+        public string SaslUsername { get; set; } = null!;
+        public string SaslPassword { get; set; } = null!;
+        public SecurityProtocol? SecurityProtocol { get; set; } = null!;
+        public SaslMechanism? SaslMechanism { get; set; } = null!;
         public int MaxBatchSize { get; set; } = 100;
         public int MaxBatchBytes { get; set; } = 102400;
+        public AutoOffsetReset? AutoOffsetReset { get; set; }
         public int PollIntervalMs { get; set; } = 10000;
         public int CommitIntervalMs { get; set; } = 5000;
         //public int ConsumerConcurrency { get; set; } = 1;
